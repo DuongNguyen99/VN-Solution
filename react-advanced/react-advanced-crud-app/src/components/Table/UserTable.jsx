94c0c3363a39b6ace505c47685ea46eaee724b06
duong.nd99 <dngngng99@gmail.com>
2021-04-14 10:28:43 +0700

    Refactor table into reusable components

--- react-advanced/react-advanced-crud-app/src/components/Table/UserTable.jsx
+++ react-advanced/react-advanced-crud-app/src/components/Table/UserTable.jsx
@@ -3,42 +3,48 @@ import { Table } from "rsuite"
 import ActionCell from "./ActionCell/ActionCell"
 
 import "rsuite/dist/styles/rsuite-default.css"
-// import styles from "./UserTable.module.css"
 
 const { Column, HeaderCell, Cell } = Table
 
-const Header = (props) => (
-  <HeaderCell
-    {...props}
-    style={{ padding: "0 10px", fontSize: "1rem", color: "black", fontWeight: 600 }}
-  />
-)
+const Header = (props) => {
+  return (
+    <HeaderCell
+      {...props}
+      style={{ padding: "0 10px", fontSize: "1rem", color: "black", fontWeight: 600 }}
+    />
+  )
+}
+
+const createTable = (data, setIsEditing, onEdit, onDelete) => {
+  return (
+    <Table data={data} autoHeight rowHeight={56}>
+      <Column flexGrow={1} verticalAlign="middle">
+        <Header>Id</Header>
+        <Cell dataKey="id" />
+      </Column>
+      <Column flexGrow={2} verticalAlign="middle">
+        <Header>Name</Header>
+        <Cell dataKey="name" />
+      </Column>
+      <Column flexGrow={3} verticalAlign="middle">
+        <Header>Username</Header>
+        <Cell dataKey="username" />
+      </Column>
+      <Column flexGrow={2} verticalAlign="middle">
+        <Header>Actions</Header>
+        <ActionCell setIsEditing={setIsEditing} onEdit={onEdit} onDelete={onDelete} />
+      </Column>
+    </Table>
+  )
+}
 
 const UserTable = (props) => {
   return (
-    <div>
-      <Table data={props.users} autoHeight rowHeight={56}>
-        <Column flexGrow={1} verticalAlign="middle">
-          <Header>Id</Header>
-          <Cell dataKey="id" />
-        </Column>
-
-        <Column flexGrow={2} verticalAlign="middle">
-          <Header>Name</Header>
-          <Cell dataKey="name" />
-        </Column>
-
-        <Column flexGrow={3} verticalAlign="middle">
-          <Header>Username</Header>
-          <Cell dataKey="userName" />
-        </Column>
-
-        <Column flexGrow={2} verticalAlign="middle">
-          <Header>Actions</Header>
-          <ActionCell onEdit={props.onEdit} onDelete={props.onDelete} />
-        </Column>
-      </Table>
-    </div>
+    <>
+      {!props.isSearching
+        ? createTable(props.users, props.setIsEditing, props.onEdit, props.onDelete)
+        : createTable(props.searchedUsers, props.setIsEditing, props.onEdit, props.onDelete)}
+    </>
   )
 }
 
