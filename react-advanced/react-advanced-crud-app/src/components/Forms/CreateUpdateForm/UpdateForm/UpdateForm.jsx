be0c787310aaf72d16af7fb64ceeffcfdfe571ba
duong.nd99 <dngngng99@gmail.com>
2021-04-14 10:30:46 +0700

    Change parameters name and update cancel function

--- react-advanced/react-advanced-crud-app/src/components/Forms/CreateUpdateForm/UpdateForm/UpdateForm.jsx
+++ react-advanced/react-advanced-crud-app/src/components/Forms/CreateUpdateForm/UpdateForm/UpdateForm.jsx
@@ -5,7 +5,6 @@ import { Form, Field } from "react-final-form"
 import styles from "../CreateUpdateForm.module.css"
 
 const UpdateForm = (props) => {
-  const initialState = { id: null, name: "", userName: "" }
   return (
     <>
       <h2>Update User</h2>
@@ -25,10 +24,10 @@ const UpdateForm = (props) => {
                   )
                 }}
               </Field>
-              <Field name="userName">
+              <Field name="username">
                 {({ input }) => (
                   <div style={{ marginBottom: "1rem" }}>
-                    <label htmlFor="userName">Username</label>
+                    <label htmlFor="username">Username</label>
                     <input {...input} className={styles["form-input"]} type="text" />
                   </div>
                 )}
@@ -37,12 +36,7 @@ const UpdateForm = (props) => {
                 <Button appearance="primary" size="lg" type="submit">
                   Update
                 </Button>
-                <Button
-                  appearance="ghost"
-                  size="lg"
-                  type="button"
-                  onClick={() => props.setEditingUser(initialState)}
-                >
+                <Button appearance="ghost" size="lg" type="button" onClick={props.onCancel}>
                   Cancel
                 </Button>
               </ButtonToolbar>
